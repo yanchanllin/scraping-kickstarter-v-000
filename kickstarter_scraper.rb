f627a8a2e0d16b1b76cbcd34b773b84866283a86
YanC Lin <yonalin1@gmail.com>
2017-11-28 21:16:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,4 +8,6 @@ kickstarter = Nokogiri::HTML(html)
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 end
